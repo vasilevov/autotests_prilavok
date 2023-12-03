ff7fafa90be2ef24c4cc22efa1a6ae06afaa879e
Oleg <vasilevovv@yandex.ru>
2023-12-03 16:07:10 +0300

    comments added

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,17 +1,15 @@
 import functions
 
 
+#Создание набора с именем из одного символа. ОК
 def test_create_kit_1_symbol_in_name():
     name = "1"
     response = functions.create_kit(name)
-    # required_fields = ["name", "productsList", "id", "productsCount"]
-    # fieldchck = functions.check_required_fields(required_fields, response)
-
     assert response.status_code == 201
     assert response.json()["name"] == name
-    # assert fieldchck is True
 
 
+#Создание набора с именем из 511 символов. ОК
 def test_create_kit_511_symbols_in_name():
     name = "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC"
     response = functions.create_kit(name)
@@ -20,20 +18,21 @@ def test_create_kit_511_symbols_in_name():
     assert response.json()["name"] == name
 
 
+#Создание набора с именем из 512 символов. Ошибка
 def test_create_kit_512_symbols_in_name():
     name = "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD"
     response = functions.create_kit(name)
 
     assert response.status_code == 400
 
-
+#Создание набора с пустым вводом в качестве имени. Ошибка
 def test_create_kit_no_symbols_in_name():
     name = ""
     response = functions.create_kit(name)
 
     assert response.status_code == 400
 
-
+#Создание набора с именем из английских букв. ОК
 def test_create_kit_english_letters_in_name():
     name = "QWErty"
     response = functions.create_kit(name)
@@ -42,6 +41,7 @@ def test_create_kit_english_letters_in_name():
     assert response.json()["name"] == name
 
 
+#Создание набора с именем из русских букв. ОК
 def test_create_kit_russian_letters_in_name():
     name = "Мария"
     response = functions.create_kit(name)
@@ -50,6 +50,7 @@ def test_create_kit_russian_letters_in_name():
     assert response.json()["name"] == name
 
 
+#Создание набора с именем со спецсимволами. ОК
 def test_create_kit_special_symbols_in_name():
     name = '"№%@",'
     response = functions.create_kit(name)
@@ -57,7 +58,7 @@ def test_create_kit_special_symbols_in_name():
     assert response.status_code == 201
     assert response.json()["name"] == name
 
-
+#Создание набора с именем с пробелами. ОК
 def test_create_kit_spaces_in_name():
     name = " Человек и КО "
     response = functions.create_kit(name)
@@ -66,6 +67,7 @@ def test_create_kit_spaces_in_name():
     assert response.json()["name"] == name
 
 
+#Создание набора с именем из строки цифр. ОК
 def test_create_kit_numbers_string_in_name():
     name = "123"
     response = functions.create_kit(name)
@@ -74,12 +76,13 @@ def test_create_kit_numbers_string_in_name():
     assert response.json()["name"] == name
 
 
+#Создание набора без имени. Ошибка
 def test_create_kit_no_name():
     response = functions.create_kit_no_name()
 
     assert response.status_code == 400
 
-
+#Создание набора с именем из цифр. Ошибка
 def test_create_kit_numbers_in_name():
     name = 123
     response = functions.create_kit(name)
--- functions.py
+++ functions.py
@@ -2,27 +2,23 @@ import data
 import sender_stand_request
 
 
+#Cоздание набора с указанием имени
 def create_kit(kit_name):
     user = sender_stand_request.post_new_user(data.default_user_body)
     return sender_stand_request.post_new_client_kit(user, create_custom_kit_body(kit_name))
 
+#Создание набора без имени
 def create_kit_no_name():
     user = sender_stand_request.post_new_user(data.default_user_body)
     return sender_stand_request.post_new_client_kit(user, data.empty_body)
+
+#Возвращает тело json для создания набора именем name
 def create_custom_kit_body(name):
     body = data.default_kit_body
     body["name"] = name
     return body
 
-def check_required_fields(fields, response):
-    check = False
-    for field in fields:
-        if field in response.json():
-            check = True
-        else:
-            check = False
-    return check
-
+#Возвращает json headers с токеном пользователя token
 def set_header_token(token):
     header = data.headers_with_token
     header["Authorization"] = token
